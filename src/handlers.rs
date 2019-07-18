061962599e348193d608bdf0eb139b156d560a98
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-18 17:43:45 -0500

    Added posthandler complete methods

--- src/handlers.rs
+++ src/handlers.rs
@@ -98,3 +98,34 @@ impl Handler for PostPostHandler {
         Ok(Response::with((status::Created, payload)))
     }
 }
+
+pub struct PostHandler {
+    database: Arc<Mutex<Database>>,
+}
+
+impl PostHandler {
+    fn new(database: Arc<Mutex<Database>>) -> PostHandler {
+        PostHandler { database }
+    }
+
+    fn find_post(&self, id:&Uuid) -> Option<Post> {
+        let locked = lock!(self.database);
+        let mut iterator = locked.posts().iter();
+        iterator.find(|p| p.uuid() == id).map(|p| p.clone())
+    }
+}
+
+impl Handler for PostHandler {
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        let ref post_id = get_http_param!(req, "id");
+
+        let id = try_handler!(Uuid::parse_str(post_id), status::BadRequest);
+
+        if let Some(post) = self.find_post(&id) {
+            let payload = try_handler!(json::encode(&post), status::InternalServerError);
+            Ok(Response::with((status::Ok, payload)))
+        } else {
+            Ok(Response::with((status::NotFound)))
+        }
+    }
+}
