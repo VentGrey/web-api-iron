1c4c5afbd85a47a4d79a58e68921afaf0b9fa91b
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-18 16:55:03 -0500

    Started adding database impls

---
+++ src/database.rs
@@ -0,0 +1,16 @@
+use models::Post;
+
+#[derive(Clone, Debug)]
+pub struct Database {
+    posts: Vec<Post>,
+}
+
+impl Database {
+    pub fn new() -> Database {
+        Database { posts: vec![] }
+    }
+
+    pub fn add_post(&mut self, post: Post) {
+        self.posts.push(post);
+    }
+}
