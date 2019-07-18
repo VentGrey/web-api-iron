8f02dad9be8d6d797676c5c4d3c9b5d46353e6aa
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-18 17:48:40 -0500

    Handling Json thingies

--- src/handlers.rs
+++ src/handlers.rs
@@ -129,3 +129,12 @@ impl Handler for PostHandler {
         }
     }
 }
+
+pub struct JsonAfterMiddleware;
+
+impl AfterMiddleware for JsonAfterMiddleware {
+    fn after(&self, _: &mut Request, mut res: Response) -> IronResult<Response> {
+        res.headers.set(ContentType::json());
+        Ok(res)
+    }
+}
